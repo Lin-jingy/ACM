58c82770c8aa4a7615d950c3193beb378de3f88f
0ljy <2221650642@qq.com>
2024-08-02 21:30:38 +0800

    update

--- my/Timer.cpp
+++ my/Timer.cpp
@@ -1,11 +1,13 @@
 #include <bits/stdc++.h>
 
+template <bool print>
 class Timer {
 private:
     std::chrono::system_clock::time_point begin;
 public:
     Timer() { begin = std::chrono::system_clock::now(); }
     ~Timer() {
+        if(!print) return ;
         auto end = std::chrono::system_clock::now();
         auto time = std::chrono::duration<double, std::nano>(end - begin).count();
         std::clog << "\nThe program takes: " << time / 1e9 << " s\n";
--- template/Tarjan.cpp
+++ template/Tarjan.cpp
@@ -1,37 +1,70 @@
 #include <bits/stdc++.h>
 
-
-
-static std::vector<std::vector<int>> 
-Tarjan(std::vector<std::vector<int>> &v) {
-    int cnt = 0;
-    std::vector<std::vector<int>> component;
-    std::vector<int> dfn(v.size()), low(v.size());
-    std::vector<bool> instack(v.size());
+/** 
+ * @brief 求强连通分量
+ * @param v 图
+ * @note  O(n) 针对有向图
+ * @return 割点编号
+ */
+static std::vector<int> Tarjan(std::vector<std::vector<int>> &v) {
+    int cnt = 0, f = 0;
+    std::vector<int> dfn(v.size()), low(v.size()), belong(v.size());
     std::stack<int> s;
-    auto tarjan = [&](auto self, int p) ->void {
-        dfn[p] = low[p] = ++cnt;
+    auto tarjan = [&](auto self, int p) -> void {
+        dfn[p] = low[p] = ++cnt;  // 给时间戳
         s.push(p);
-        instack[p] = true;
-        for (auto i:v[p]) {
-            if (!dfn[i]) {
+        for (auto i : v[p]) {
+            if (!dfn[i]) {  // 未访问过
                 self(self, i);
                 low[p] = std::min(low[p], low[i]);
-            } else if (instack[i]) {
+            } else if (!belong[i]) {
                 low[p] = std::min(low[p], dfn[i]);
             }
         }
         if (dfn[p] == low[p]) {
-            component.push_back({});
+            ++f;
             int node;
             do {
                 node = s.top();
                 s.pop();
-                instack[node] = false;
-                component.back().push_back(node);
+                belong[node] = f;
             } while (node != p);
         }
     };
     tarjan(tarjan, 1);
-    return component;
+    return belong;
+}
+
+/** 
+ * @brief 求割点
+ * @param v 图
+ * @param n 点数
+ * @note  O(n) 针对有向图
+ * @return 割点编号
+ */
+static std::vector<int> Tarjan(std::vector<std::vector<int>> &v, int n) {
+    int cnt = 0;
+    std::vector<int> dfn(v.size()), low(v.size());
+    std::vector<int> result;
+    int root = 0;
+    auto tarjan = [&](auto self, int p) -> void {
+        dfn[p] = low[p] = ++cnt;
+        int son = 0;
+        bool isG = 0;
+        for (auto i : v[p]) {
+            if (!dfn[i]) {
+                ++son;
+                self(self, i);
+                low[p] = std::min(low[p], low[i]);
+                if (low[i] >= dfn[p] and p != root) isG = 1;
+            } else
+                low[p] = std::min(low[p], dfn[i]);
+        }
+        if (son >= 2 and p == root) isG = 1;
+        if (isG) result.push_back(p);
+    };
+    for (int i = 1; i <= n; ++i) {
+        if(!dfn[i]) root = i, tarjan(tarjan, i);
+    }
+    return result;
 }
